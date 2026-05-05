9ebdc22d9e0b70dfb21201d71d67db92f88759ed
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-05-05 16:59:57 +0700

    fix: button props not spreading

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,13 +2,14 @@ import { cn } from "@/libs/utils";
 import { Arrow } from "./Arrow";
 import { RollingLabel } from "./Label";
 
-export const Button = ({ children, className }: React.ComponentProps<"button">) => {
+export const Button = ({ children, className, ...props }: React.ComponentProps<"button">) => {
   return (
     <button
       className={cn(
         "group flex items-center gap-2 uppercase bg-primary text-text-dark leading-4 text-xs px-4.5 py-3 rounded-lg cursor-pointer hover:scale-95 hover:bg-secondary hover:text-text-light transition-all duration-700 ease-[cubic-bezier(0.87,0,0.13,1)]",
         className,
       )}
+      {...props}
     >
       <RollingLabel>{children}</RollingLabel>
       <Arrow className="group-hover:-rotate-45 group-hover:fill-text-light transition-all duration-700 ease-[cubic-bezier(0.87,0,0.13,1)]" />
