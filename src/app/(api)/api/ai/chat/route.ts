0f901017ca3ab39bff180346e2a3d7e8e664d378
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-05-17 20:10:13 +0700

    fix: missing error recover

--- src/app/(api)/api/ai/chat/route.ts
+++ src/app/(api)/api/ai/chat/route.ts
@@ -1,5 +1,6 @@
 import { isDevEnv } from "@/config";
 import { AIController } from "@/controllers/ai";
+import { ServerErrorRecover, statusMap } from "@/libs/error/server/recover";
 import { AIMiddleware } from "@/middlewares/ai";
 import { AIPayload } from "@/payloads/ai";
 import { createAppRouter } from "nectic/route";
@@ -8,5 +9,5 @@ export const { POST } = createAppRouter(
   {
     POST: [AIMiddleware.extractConversationId, AIController.chat],
   },
-  { POST: AIPayload.chat, cors: true, debugMode: isDevEnv() },
+  { POST: AIPayload.chat, cors: true, debugMode: isDevEnv(), recover: ServerErrorRecover, statusMap },
 );
---
+++ src/libs/error/server/recover.ts
@@ -0,0 +1,27 @@
+import { createReply, Reply } from "nectic/server";
+import { ServerError } from ".";
+import { AppRouterHandler, RecoverFunc } from "nectic/route";
+import { isDevEnv } from "@/config";
+
+export const statusMap: {
+  code: string[];
+  status: number;
+}[] = [
+  { code: ["INVALID_AUTH", "INVALID_TOKEN"], status: 401 },
+  { code: ["FORBIDDEN"], status: 403 },
+  { code: ["NOT_FOUND"], status: 404 },
+  { code: ["CLIENT_FIELD", "MISSING_FIELDS"], status: 406 },
+  { code: ["IS_RECYCLED", "NOT_RECYCLED", "CONFLICT"], status: 409 },
+  { code: ["TOO_MUCH_REQUEST"], status: 429 },
+  { code: ["SERVER_ERROR"], status: 500 },
+  { code: ["BAD_GATEWAY"], status: 502 },
+];
+
+export const ServerErrorRecover: RecoverFunc<AppRouterHandler> = async (err, req, res) => {
+  const reply = createReply({ req, res, statusMap, debugMode: isDevEnv() });
+  if (err instanceof ServerError) {
+    return reply.error(await err.flatten()).fail();
+  } else {
+    return reply.error({ code: "SERVER_ERROR", message: "Unknown error" }).fail();
+  }
+};
