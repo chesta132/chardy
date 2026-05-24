feef7fcb1213ced7b556870436def6bdcbd7ba88
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-05-24 20:21:08 +0700

    cleanup: remove notify files notifyError not used anymore

--- src/actions/notify.ts
+++
@@ -1,6 +0,0 @@
-"use server";
-
-import { notifyError } from "@/services/notify";
-import { createNectAction } from "nectic/actions";
-
-export const notifyErrorAction = createNectAction().handle(notifyError);
