a01c6e97fc9cdba8c6d51cf0b8d31f28bd9d8804
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-05-28 16:33:33 +0700

    feat: update nectic and apply nect proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,29 +2,40 @@ import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 import { NextRequest, NextResponse } from "next/server";
 import { ADMIN_URL, APP_URL, INTERNAL_ADMIN_PATH } from "./config";
+import { createNectProxy } from "nectic/proxy";
 
-const intlMw = createMiddleware(routing);
+const proxy = createNectProxy();
 
-export default async function proxy(request: NextRequest) {
-  const host = request.headers.get("host") || "";
-  const adminUrl = new URL(ADMIN_URL);
-  const appUrl = new URL(APP_URL);
-
-  // make sure it does not break when admin url === app url
-  const isAdminHost = host === adminUrl.host && host !== appUrl.host;
-
-  if (isAdminHost) {
-    const url = request.nextUrl.clone();
+proxy.use(
+  (req) => {
+    const url = new URL(req.nextUrl);
     // rewrite to payload cms ui path
     url.pathname = `${INTERNAL_ADMIN_PATH}${url.pathname}`;
     return NextResponse.rewrite(url);
-  }
+  },
+  {
+    matcher: (req) => {
+      const host = req.getHeader("host") || "";
+      const adminUrl = new URL(ADMIN_URL);
+      const appUrl = new URL(APP_URL);
 
-  const shouldSkipIntl = ADMIN_URL === APP_URL && request.nextUrl.pathname.startsWith(INTERNAL_ADMIN_PATH);
+      // make sure it does not break when admin url === app url
+      const isAdminHost = host === adminUrl.host && host !== appUrl.host;
+      return isAdminHost;
+    },
+  },
+);
 
-  if (shouldSkipIntl) return NextResponse.next();
-  return intlMw(request);
-}
+const intlMw = createMiddleware(routing);
+proxy.use((req) => intlMw(req.raw as NextRequest), {
+  // if admin url is app url and pathname not start with INTERNAL_ADMIN_PATH
+  matcher: (req) => !(ADMIN_URL === APP_URL && req.nextUrl.pathname.startsWith(INTERNAL_ADMIN_PATH)),
+});
+
+// best practice to return .next()
+proxy.use(() => NextResponse.next());
+
+export default proxy.handle();
 
 export const config = {
   matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
