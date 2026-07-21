e18bb299797fa0e305dc4875de1fbd86910662f0
Chesta Ardiona <chestaardi4@gmail>
2026-07-21 14:06:33 +0700

    fix: marquee not moving on no motion

--- src/components/about-me/ToolsMarquee.tsx
+++ src/components/about-me/ToolsMarquee.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { cn } from "@/libs/utils";
 import { usePreference } from "@/contexts/Preference";
 import { FullMarquee, MarqueeProps, SimpleMarquee } from "../ui/Marquee";
 
@@ -8,23 +7,17 @@ export const Marquee = ({ children, speed = 40, className }: MarqueeProps) => {
   const { motion } = usePreference();
 
   switch (motion) {
-    case "no-motion":
-      return (
-        <div className={cn("overflow-hidden w-full", className)}>
-          <div className="flex w-max">{children}</div>
-        </div>
-      );
-    case "reduce":
-      return (
-        <SimpleMarquee speed={speed} className={className}>
-          {children}
-        </SimpleMarquee>
-      );
-    default:
+    case "full":
       return (
         <FullMarquee speed={speed} className={className}>
           {children}
         </FullMarquee>
+      );
+    default:
+      return (
+        <SimpleMarquee speed={speed} className={className}>
+          {children}
+        </SimpleMarquee>
       );
   }
 };
