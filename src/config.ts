1fbaab49a0954bc88dc448411f25582da1461706
Chesta Ardiona <chestaardi4@gmail.com>
2026-07-21 15:00:27 +0700

    fix: failed to deploy cs my git config error

--- src/config.ts
+++ src/config.ts
@@ -49,3 +49,4 @@ export const INTERNAL_ADMIN_PATH = "/admin"; // following src/app/(payload)/admi
 // helpers
 export const isDevEnv = () => process.env.NODE_ENV === "development";
 export const isProdEnv = () => process.env.NODE_ENV === "production";
+
